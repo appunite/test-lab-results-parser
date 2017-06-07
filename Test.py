d1709d106f200659c3d9c4b079ecf0189df6ebd1
piotr_madry <piotr.madry@appunite.com>
2017-06-07 23:13:48 +0200

    use element tree to find needed properties

---
+++ Test.py
@@ -0,0 +1,10 @@
+class Test(object):
+
+	name = ""
+	uid = 0
+	passed = False
+
+	def __init__(self, name, uid, passed):
+		self.name = name
+		self.uid = uid
+		self.passed = passed
--- parse.py
+++ parse.py
@@ -1,17 +1,24 @@
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
+from Test import Test
 import os
-# resultsDir = '/Users/piotrmadry/Desktop/work/reports/firebase-test-lab/hammerhead-21-en-portrait/test_result_3697305487943705087.xml'
-# DOMTree = minidom.parse(resultsDir)
-# print DOMTree.toxml()
 
-def iterateThroughResults():
+def prepareListOfTestResults():
 	resultsDir = '/Users/piotrmadry/Desktop/work/reports/firebase-test-lab/'
+	testList = []
 	for subdir, dirs, files in os.walk(resultsDir):
         	for file in files:
         		filePath = os.path.join(subdir, file)
         		if (filePath.endswith('.xml')):
-        			DOMTree = minidom.parse(filePath)
-        			print DOMTree.toxml()
-	pass
+        			document = ET.parse(filePath)
+        			root = document.getroot()
+        			print 'Passed: ' + str((root.attrib['failures'] == '0' and root.attrib['errors'] == '0'))
+        			testcase = document.find('testcase')
+        			print 'Name: ' + testcase.attrib['name'] 
 
-iterateThroughResults()
+	return testList
+
+def main():
+  testList = prepareListOfTestResults()
+
+if __name__ == "__main__": 
+  main()
