89ca4c1e8d1f6bd539726f1dfdec51e6e21f0d42
piotr_madry <piotr.madry@appunite.com>
2017-06-08 13:26:24 +0200

    add explicit arguments to script

--- testlab.py
+++ testlab.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 from utils import getResults, createJsonFile
 
 def toJson(object):
@@ -6,10 +7,16 @@ def toJson(object):
             sort_keys=True, indent = 4)
 
 def main():
+	parser = argparse.ArgumentParser(description='Process files paths')
+    	parser.add_argument('--source', dest='source', required=True, nargs='?', type=str, help='source')
+    	parser.add_argument('--destination', dest='destination', required=True, nargs='?', type=str, help='destination')
+    	args = parser.parse_args()
+
 	resultsDir = '/Users/piotrmadry/Desktop/work/reports/firebase-test-lab/'
-	results = getResults(resultsDir)
 	destinationPath = '/Users/piotrmadry/Desktop/'
-	createJsonFile(destinationPath, toJson(results))
+
+	results = getResults(args.source)
+	createJsonFile(args.destination, toJson(results))
 
 if __name__ == "__main__": 
   main()
