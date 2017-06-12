befa9078dca2fb5299e9c84db0d1935e835c363b
piotr_madry <piotr.madry@appunite.com>
2017-06-12 14:06:39 +0200

    update file name

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import codecs, json
 import xml.etree.ElementTree as ET
 from model import Device, Tests, Test
 
-	
+
 def prepareTestLists(files, subdir):
 	passedTestList = []
 	failedTestList = []
@@ -22,7 +22,7 @@ def prepareTestLists(files, subdir):
 	        	else:
 	        		failedTestList.append(test)
 
-	return passedTestList, failedTestList	
+	return passedTestList, failedTestList
 
 def getDeviceMeta(subdir):
 	return os.path.basename(subdir).split('-')
@@ -30,23 +30,21 @@ def getDeviceMeta(subdir):
 def createJsonFile(destinationPath, json):
 	if not os.path.exists(destinationPath):
 		os.mkdir(destinationPath)
-	filePath = os.path.join(destinationPath, 'test-results.json')
+	filePath = os.path.join(destinationPath, 'results.json')
 	file = open(filePath, 'w+')
 	file.write(json)
 	file.close()
 	pass
 
-	
+
 def getResults(resultsDir):
 	testResults = []
 	for subdir, dirs, files in os.walk(resultsDir, topdown=True):
-			passedTestList, failedTestList = prepareTestLists(files, subdir)	
+			passedTestList, failedTestList = prepareTestLists(files, subdir)
 			if passedTestList or failedTestList:
 				deviceMeta = getDeviceMeta(subdir)
 				tests = Tests(passedTestList, failedTestList)
 				device = Device(deviceMeta[0], "1000", "1.0", "1000", deviceMeta[3], deviceMeta[2], "10:30", "date", "Piotr Madry", "Android, " + deviceMeta[1], tests)
 				testResults.append(device)
-        	
-	return testResults
 
-	
+	return testResults
